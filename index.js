f50219180d5731c10a779679681e931bff436489
Danking <toth.daniel.1990@gmail.com>
2015-05-27 16:15:12 +0200

    new get api implemented

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/server_state', function (request, response) {
     });
 });
 
-app.get('/', function (request, response) {
+app.get('/yo_world', function (request, response) {
     response.send('Hello World!');
 });
 
